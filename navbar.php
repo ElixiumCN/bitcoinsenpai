db5b3b4362a40ad24569aabbd9aa804549d7a10c
ElixiumCN <100935972+ElixiumCN@users.noreply.github.com>
2022-03-15 11:08:01 +0000

    Hi

---
+++ navbar.php
@@ -0,0 +1,16 @@
+<div class="colordiv">
+    <div id="top"></div>
+    <div id="navbar" class="center"> 
+<a href="index.html">Home</a>
+<a href="bybit.html">Register with Bybit</a>
+<a href="disclaimer.html">Disclaimer & Risk Disclosure</a>
+<a href="socialmedia.html">Social Media</a>
+<a href="ma1.html">Moving Averages #1</a>
+<a href="ma2.html">Moving Averages #2</a>
+<a href="sr.html">Support & Resistance</a>
+<a href="rsi.html">Relative Strength Index</a>
+<a href="fibonacci.html">Fibonacci Retracement</a>
+<a href="term.html">Trade Execution & Risk Management</a>
+<a href="iof.html">Institutional Order Flow</a>
+<a href="tss.html">The Short Squeeze</a>
+    </div>
---
+++ socialmedia.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    
+    <title></title>
+</head>
+<body>
+<?php include 'navbar.php';?>
+
+
+    <p class="center white">Follow Bitcoin Senpai on Social Media</p>
+
+    <div class="center white widthtext">
+    <a href="https://twitter.com/BitcoinSenpai" target="_blank">Bitcoin Senpai on Twitter</a>
+    <br>
+    <a href="https://www.youtube.com/channel/UCzIFPo--WqHNafuhfiw-yKQ/featured" target="_blank">Bitcoin Senpai on YouTube</a>
+
+    <br><br>
+    <img src="senpai.jpg" alt="senpai avatar">
+            </div>
+    
+<video id="background-video" autoplay loop muted poster="">
+    <source src="senpai.mp4" type="video/mp4">
+
+</div>
+</body>
+</html>
